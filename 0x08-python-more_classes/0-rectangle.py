117a694c8341071a89b4833da4bbabe62ab6f0b3
Nick <nickigber@gmail.com>
2023-01-04 03:24:52 -0800

    rectangle

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,30 +1,9 @@
-#!/usr/bin/python3
+#!/usr/bin/pytho#!/usr/bin/python3
 
-"""
-Module for defining a rectangle with a given width and height.
 
-Classes:
-- Rectangle: defines a rectangle with a given width and height.
-"""
+'''Defines a Rectangle class.'''
 
 
 class Rectangle:
-
-    """
-    Defines a rectangle with a given width and height.
-
-    Attributes:
-    - width (int): The width of the rectangle.
-    - height (int): The height of the rectangle.
-    """
+    '''Represents a rectangle.'''
     pass
-
-
-# Create an instance of the Rectangle class
-my_rectangle = Rectangle()
-
-# Print the type of the instance
-print(type(my_rectangle))
-
-# Print the instance's __dict__ attribute
-print(my_rectangle.__dict__)
