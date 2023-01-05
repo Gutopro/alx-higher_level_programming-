77dd96a5caa01fa625f5b414648ae0fb9bce9c11
Nick <nickigber@gmail.com>
2023-01-05 00:29:56 -0800

    search

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    # Create a new list
+    new_list = []
+
+    # Iterate through each element in the list
+    for element in my_list:
+        # If the element is the search element, append the replace element to the new list
+        if element == search:
+            new_list.append(replace)
+        # Otherwise, append the element to the new list
+        else:
+            new_list.append(element)
+
+    return new_list
