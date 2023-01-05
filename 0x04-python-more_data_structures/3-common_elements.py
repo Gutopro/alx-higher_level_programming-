474e59c8d31dba0e6982408ceeabc82163df2c63
Nick <nickigber@gmail.com>
2023-01-05 00:43:29 -0800

    3-common

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    # Initialize an empty set to store the common elements
+    common = set()
+
+    # Iterate through the elements in set_1
+    for element in set_1:
+        # If the element is also in set_2, add it to the common set
+        if element in set_2:
+            common.add(element)
+
+    # Return the set of common elements
+    return common
