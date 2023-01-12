18e24995ea681b525165dd9856a14745c4415059
Nick <nickigber@gmail.com>
2023-01-12 13:26:05 +0000

    py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 # 0-square.py
- """This class defines a square object"""
+"""This class defines a square object"""
 
 class Square:
     """Represents a square"""
     pass
+if __name__ == "__main__":
+    Square = __import__('0-square').Square
+
+    my_square = Square()
+    print(type(my_square))
+    print(my_square.__dict__)
