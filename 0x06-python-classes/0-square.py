b3ca0445a178f08361ff2c2f210ca8ec1b57ab66
Nick <nickigber@gmail.com>
2023-01-20 12:58:52 +0000

    py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -5,17 +5,5 @@
 class Square:
     """
     Represents a square
-    
-    Attributes:
-        None
     """
     pass
-if __name__ == "__main__":
-    """
-    imports Square class from 0-square
-    """
-    Square = __import__('0-square').Square
-
-    my_square = Square()
-    print(type(my_square))
-    print(my_square.__dict__)
