e2f8ce1e1b5b0a41d8954fefd98fb14da94e7d03
nick <nickigber@gmail.com>
2023-02-02 02:12:15 -0800

    py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
+"""MyList
+"""
 
-""" a class MyList that inherits from list"""
 
 class MyList(list):
-    """ a subclass of list"""
-    
-    def _init_(self):
-        
-        """ initialize oblect """
-        super()._init_()
-        
-    """ MyList class has a single public instance method"""
+    """Contains list
+    """
+
     def print_sorted(self):
+        """Prints self in sorted format
+        """
+
         print(sorted(self))
