a90d0752a992511a6c4a84425a183e6ebca3f515
Nick <nickigber@gmail.com>
2023-02-22 06:40:34 -0800

    error fix

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
-'''A module containing IO functions.
-'''
+"""write_file
+"""
 
 
 def write_file(filename="", text=""):
-    '''Writes a UTF-8 encoded text to a file.
-    Args:
-        filename (str): The name of the file to write to.
-        text (str): The content to store in the file.
-    Returns:
-        int: The number of characters written.
-    '''
-    n = 0
-    with open(filename, mode='w', encoding='utf-8') as file:
-        n = file.write(text)
-    return 
+    """Takes str filename to read, and str text to write to
+    """
+
+    with open(filename, mode="w", encoding="utf-8") as writeFile:
+        writeFile.write(text)
+        return len(text)
