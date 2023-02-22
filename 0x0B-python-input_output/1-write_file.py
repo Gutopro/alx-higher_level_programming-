c61405db58d44a46eea38d355f96bba31df0b1f4
Nick <nickigber@gmail.com>
2023-02-22 06:43:14 -0800

    error fix

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
-"""write_file
-"""
 
 
-def write_file(filename="", text=""):
-    """Takes str filename to read, and str text to write to
-    """
-
-    with open(filename, mode="w", encoding="utf-8") as writeFile:
-        writeFile.write(text)
-        return len(text)
+def number_of_lines(filename=""):
+    with open(filename, "r", encoding="UTF-8") as f:
+        return len(list(f))
