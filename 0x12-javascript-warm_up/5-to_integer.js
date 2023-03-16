c53c87e0a6d140eb6df2b8008709eaf1eadfd1c0
Nicholas <nickigber@gmail.com>
2023-03-16 10:50:35 +0000

    update fix for argument to integer using parseInt and process.argv

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
-const args = process.argv[2];
-if (/^\d+$/.test(args)) {
-  console.log(`My Number: ${parseInt(args)}`);
-} else if (/^\d*\.\d+$/.test(args)) {
-  console.log(`My Number: ${(parseInt(args))}`);
+let args = parseInt(process.argv[2]);
+if (args) {
+  console.log('My Number: ' + args);
 } else {
   console.log('Not a Number');
 }
