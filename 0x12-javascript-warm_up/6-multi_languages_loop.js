9c0ed08e84763c83b853fc549238e2c70adbc6f0
Nicholas <nickigber@gmail.com>
2023-03-16 23:25:05 +0000

    fix for script that prints multiple lines using a for loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const text = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const text = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < text.length; i++) {
   console.log(text[i]);
 }
