c12fdf04947dd34b32d3608e381a557ad8485fc7
Nicholas <nickigber@gmail.com>
2023-03-16 23:56:23 +0000

    fix script that converts an argument to an integer and prints a message using a for loop and process.argv

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,7 +4,7 @@ const args = process.argv[2];
 if (/^\d+$/.test(args)) {
   const x = parseInt(args);
   for (let i = 0; i < x; i++) {
-  console.log('C is fun');
+    console.log('C is fun');
   }
 } else {
   console.log('Missing number of occurences');
