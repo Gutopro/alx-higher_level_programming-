76edf56c781936fb94b49234a12359417fa1e1ed
Nicholas <nickigber@gmail.com>
2023-03-17 00:37:41 +0000

    function that returns a factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+function factorial (n) {
+  if (isNaN(n)) {
+    return 1;
+  }
+  if (n === 0) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+const args = process.argv.slice(2);
+const num = parseInt(args[0]);
+console.log(factorial(num));
