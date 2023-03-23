86a285bb9bcec86c0af98bfb0f5d2883d2cea644
Nicholas <nickigber@gmail.com>
2023-03-23 10:19:12 +0000

    fix class rectangle with height and width propertise

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 // js Script
-module.exports = class Rectangle {
+class Rectangle {
   constructor (h, w) {
     this.height = h;
     this.width = w;
   }
-};
+}
+module.exports = Rectangle;
