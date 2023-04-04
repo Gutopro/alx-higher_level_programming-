41ea18ee1dd5defe02461aba8988133a5728c134
Nick <nickigber@gmail.com>
2023-04-04 13:55:52 +0100

    script that takes in a URL, sends a request to the URL and displays the value of the X-Request-Id variable found in the header of the response.

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,14 +1,28 @@
-#!/usr/bin/python3
-import urllib.request
+#!/usr/bin/env python3
+
+"""
+This script sends a request to a given URL and prints the value of the X-Request-Id
+variable found in the header of the response.
+"""
+
 import sys
+import urllib.request
 
-# Get the URL from the command line arguments
-url = sys.argv[1]
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: {} <URL>".format(sys.argv[0]))
+        sys.exit(1)
 
-# Open the URL using a with statement
-with urllib.request.urlopen(url) as response:
-    # Get the value of the X-Request-Id header
-    x_request_id = response.getheader('X-Request-Id')
+    url = sys.argv[1]
 
-    # Display the X-Request-Id header value
-    print(x_request_id)
+    request = urllib.request.Request(url)
+
+    try:
+        with urllib.request.urlopen(request) as response:
+            headers = dict(response.headers)
+            x_request_id = headers.get("X-Request-Id")
+            if x_request_id:
+                print(x_request_id)
+    except urllib.error.URLError as e:
+        print("Error fetching page: {}".format(e.reason))
+        sys.exit(1)
