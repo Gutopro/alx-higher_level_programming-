5de448100482852310889bf053a700ac3a671758
Nick <nickigber@gmail.com>
2023-04-27 08:00:32 +0100

    web scraping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,12 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (err, contents) {
-  if (contents === undefined) {
-    console.log(err);
-  } else {
-    console.log(contents);
-  }
+
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+	console.error(err);
+	return;
+    }
+    console.log(data);
 });
